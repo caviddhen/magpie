b3f98c51bff20808942fcf2da148b0e3d33510b3
david chen <cavid.dhen@gmail.com>
2020-10-05 13:50:58 +0200

    "yled dcrs start script"

---
+++ scripts/start/projects/yield_decrease.R
@@ -0,0 +1,35 @@
+# |  (C) 2008-2019 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+# Source the default config and then over-write it before starting the run.
+source("config/default.cfg")
+
+# Change results folder name
+cfg$results_folder <- "output/:title:"
+
+
+
+			# Update title
+			cfg$title <- paste0("Run","_","yld_dcrs")
+			cfg$gms$s14_yld_dcrs_switch  <- "factor"
+			start_run(cfg=cfg)
+
+			# Update title
+			cfg$title <- paste0("Run","_","no_yld_dcrs")
+			cfg$gms$s14_yld_dcrs_switch <- "normal"
+
+			# Start the run
+			start_run(cfg=cfg)
+

8e1001a4091bfdb7896acd7790e47240f8dd9e20
david chen <cavid.dhen@gmail.com>
2020-10-19 13:05:46 +0200

    update degrowth start script

--- scripts/start/projects/degrowth.R
+++ scripts/start/projects/degrowth.R
@@ -17,6 +17,7 @@ source("config/default.cfg")
 
 cfg$force_download <- TRUE
 
+
 cfg$repositories <- append(list("https://rse.pik-potsdam.de/data/magpie/public"=NULL,
                                 "./patch_inputdata"=NULL),
                            getOption("magpie_repos"))
@@ -28,7 +29,7 @@ cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev48_c200_690d3718e151be1b45
                                     "additional_data_rev3.86.tgz",
                "patch_degr_sust_scale.tgz")
 
-cfg$title <- paste0("Degr_14_sust_scale")
+cfg$title <- paste0("Degr_19_sust_scale")
 
 #start run
 start_run(cfg=cfg)
@@ -41,37 +42,43 @@ cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev48_c200_690d3718e151be1b45
                                     "calibration_H12_c200_26Feb20.tgz",
                                     "additional_data_rev3.86.tgz",
                                     "patch_degr_fair_dist.tgz")
-cfg$title <- paste0("Degr_14_fair_dist")
+cfg$title <- paste0("Degr_19_fair_dist")
 start_run(cfg=cfg)
 
 
 
-#### GHG pricing w/ fair distrib
+#### GHG pricing w/ fair distrib -sust degr
 
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
-cfg$title <- paste0("Degr_14_fair_eff")
+cfg$title <- paste0("Degr_19_sust_degr")
 start_run(cfg=cfg)
 
+### everything with EAT Lancet
+cfg$cfg$gms$s15_exo_diet <- 1
+cfg$gms$c15_exo_scen_targetyear <- "y2030"
+cfg$gms$s15_exo_waste <- 1
+cfg$gms$s15_waste_scen <- 1.1
 
-### GHG Pricing only
+cfg$title <- paste0("Degr_19_trans")
+
+
+### EAT only
 cfg$input <- c("isimip_rcp-IPSL_CM5A_LR-rcp2p6-co2_rev48_c200_690d3718e151be1b450b394c1064b1c5.tgz",
                                     "rev4.52_h12_magpie.tgz",
                                     "rev4.52_h12_validation.tgz",
                                     "calibration_H12_c200_26Feb20.tgz",
                                     "additional_data_rev3.86.tgz")
-cfg$title <- paste0("Degr_14_ghg_price")
-start_run(cfg=cfg)
 
-
-## EAT Lancet
-cfg$gms$s15_exo_diet <- 1
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-NPi"
-cfg$title <- paste0("Degr_14_pref_change")
+cfg$title <- paste0("Degr_19_EAT")
 start_run(cfg=cfg)
 
-### EAT Lancet and GHG pricing
 
-cfg$gms$s15_exo_diet <- 1
+## GHG only
+
+cfg$cfg$gms$s15_exo_diet <- 0
+cfg$gms$s15_exo_waste <- 0
 cfg$gms$c56_pollutant_prices <- "R2M41-SSP2-Budg600"
-cfg$title <- paste0("Degr_14_sust_change")
+
+$title <- paste0("Degr_19_ghg_price")
 start_run(cfg=cfg)

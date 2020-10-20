29709b561ae6f12a423d3dcb5cd85efb5b7a58e2
david chen <cavid.dhen@gmail.com>
2020-10-20 10:23:37 +0200

    update config

---
+++ scripts/start/projects/labour_prod_debug.R
@@ -0,0 +1,26 @@
+# |  (C) 2008-2019 Potsdam Institute for Climate Impact Research (PIK)
+# |  authors, and contributors see CITATION.cff file. This file is part
+# |  of MAgPIE and licensed under AGPL-3.0-or-later. Under Section 7 of
+# |  AGPL-3.0, you are granted additional permissions described in the
+# |  MAgPIE License Exception, version 1.0 (see LICENSE file).
+# |  Contact: magpie@pik-potsdam.de
+
+
+######################################
+#### Script to start a MAgPIE run ####
+######################################
+
+# Load start_run(cfg) function which is needed to start MAgPIE runs
+source("scripts/start_functions.R")
+
+# Source the default config and then over-write it before starting the run.
+source("config/default.cfg")
+
+# Change results folder name
+cfg$results_folder <- "output/:title:"
+
+
+
+			# Update title
+			 cfg$title <- paste0("LabProd_debug_20","_","default")
+			 start_run(cfg=cfg)

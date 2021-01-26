5b9402978986ee5272a3d549ebcfe5ed1c424482
david chen <cavid.dhen@gmail.com>
2021-01-26 10:39:59 +0100

    yieldtest script inputs

--- scripts/start/yieldtest.R
+++ scripts/start/yieldtest.R
@@ -15,10 +15,11 @@ source("scripts/start_functions.R")
 source("config/default.cfg")
 
 cfg$input <- c("rev4.51+mrmagpie10_yieldtest_h12_5a23c697b74f282b13acf0cdc4b8f06f_cellularmagpie.tgz",
-              "rev4.51+mrmagpie10_yieldtest_h12_magpie.tgz",
-             "rev4.51+mrmagpie10_yieldtest_h12_validation.tgz",
-             "calibration_H12_c200_26Feb20.tgz",
-              "additional_data_rev3.88.tgz")
+"rev4.52_h12_magpie.tgz",
+ "rev4.51+mrmagpie10_h12_validation_debug.tgz",                                     
+ "additional_data_rev3.92.tgz")
+
+
 
 cfg$gms$forestry  <- "static_sep16"
 #start MAgPIE run
